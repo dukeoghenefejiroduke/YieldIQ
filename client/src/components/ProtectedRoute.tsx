dc12e1f16b2f52e8205881a69eeb848813b31ac3
dukeoghenefejiroduke <dukeoghenefejiroduke@gmail.com>
2026-05-19 18:47:21 +0100

    err fix

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuthStore } from '../store/authStore';
 
-export const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+export const ProtectedRoute = ({ children }: { children: React.ReactNode }): React.JSX.Element => {
   const { user } = useAuthStore();
-  return user ? children : <Navigate to="/login" replace />;
+  return user ? <>{children}</> : <Navigate to="/login" replace />;
 };
