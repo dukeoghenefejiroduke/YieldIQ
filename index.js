64e40d820df9bda53d5a1691c73b2eaa26dbd32d
dukeoghenefejiroduke <dukeoghenefejiroduke@gmail.com>
2026-05-19 23:41:54 +0100

    Your update message here

---
+++ index.js
@@ -0,0 +1,6 @@
+/**
+ * Render Entry Point
+ * This file allows Render to start the server using its default 'node index.js' command
+ * if the 'npm start' command is not used or misconfigured.
+ */
+require('./server/dist/index.js');
